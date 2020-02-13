21410960799f4577485e58582f635b7fd3ffbb73
Joey Segura <10735119@my.uvu.edu>
2020-02-12 19:48:09 -0700

    characterMovement

---
+++ unity3d-ronnyGame/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public float moveSpeed = 2f;
+
+    public Rigidbody rb;
+    public Animation anim;
+
+    private Vector3 movement;
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.z = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
