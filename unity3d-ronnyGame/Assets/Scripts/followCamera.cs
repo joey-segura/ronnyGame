cae69605e31df0eba2d347461bc62e0e8a4eeaf2
Joey Segura <10735119@my.uvu.edu>
2020-02-14 18:06:57 -0700

    CameraWork

---
+++ unity3d-ronnyGame/Assets/Scripts/followCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
+
+public class followCamera : MonoBehaviour
+{
+    public Transform focus;
+    public float followSpeed = 10f;
+    public Vector3 distance;
+    
+    void Start()
+    {
+        focus = GameObject.Find("testChar_001").transform;
+    }
+
+
+    private void LateUpdate()
+    {
+        Vector2 desiredPosition = focus.position + distance;
+        Vector2 smoothedPosition = Vector2.Lerp(transform.position, desiredPosition, followSpeed * Time.deltaTime);
+        transform.position = smoothedPosition;
+    }
+}
