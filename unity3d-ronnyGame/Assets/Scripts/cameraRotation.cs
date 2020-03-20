8d696b9b1addeb3a85e07a4bd02598466b2539e3
ritter <probsttanner@gmail.com>
2020-03-20 16:28:58 -0600

    updated unity version 2018.4 and camera rotation to key inputs

--- unity3d-ronnyGame/Assets/Scripts/cameraRotation.cs
+++ unity3d-ronnyGame/Assets/Scripts/cameraRotation.cs
@@ -4,10 +4,54 @@ using UnityEngine;
 
 public class cameraRotation : MonoBehaviour
 {
-    public float speed = 10f;
+    public float speed;
+    private Vector3 lastMousePos;
 
 void Update()
     {
+        if (Input.GetKey(KeyCode.A))
+        {
+            speed = 50f;
+        } else if (Input.GetKey(KeyCode.D))
+        {
+            speed = -50f;
+        } else if (Input.GetMouseButton(1))
+        {
+            Vector3 mousePos = Input.mousePosition;
+            if (lastMousePos != null)
+            {
+                if (mousePos.x > lastMousePos.x || mousePos.x > Screen.width - (Screen.width / 6))
+                {
+                    speed = 50f;
+                } else if (mousePos.x < lastMousePos.x || mousePos.x < Screen.width / 6)
+                {
+                    speed = -50f;
+                } else
+                {
+                    if (Mathf.Abs(speed) < 2)
+                    {
+                        speed = 0f;
+                    } else
+                    {
+                        speed = speed / 1.009f;
+                    }
+                }
+                lastMousePos = mousePos;
+            } else
+            {
+                lastMousePos = mousePos;
+            }
+        } else
+        {
+            if (Mathf.Abs(speed) < 2)
+            {
+                speed = 0f;
+            }
+            else
+            {
+                speed = speed / 1.009f;
+            }
+        }
         transform.Rotate(0, speed * Time.deltaTime, 0);
     }
 }
