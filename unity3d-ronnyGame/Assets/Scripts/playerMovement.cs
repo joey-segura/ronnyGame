36638c5ab6832023fbb2994e1c8a1541416fb480
Joey Segura <10735119@my.uvu.edu>
2020-03-27 08:19:02 -0600

    walkScript Changes
    Moved over from rb to charController

--- unity3d-ronnyGame/Assets/Scripts/playerMovement.cs
+++ unity3d-ronnyGame/Assets/Scripts/playerMovement.cs
@@ -2,19 +2,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+[RequireComponent(typeof(CharacterController))]
 
 public class playerMovement : MonoBehaviour
 {
-    public float moveSpeed = 2f;
-
-    public Rigidbody rb;
-    public Animator anim;
-
+    private CharacterController controller;
     private Vector3 movement;
+    public float moveSpeed = 2f;
+    public Animator anim;
+    
+    //public Rigidbody rb;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.z = Input.GetAxisRaw("Vertical");
+        controller.Move(movement * Time.deltaTime);
+
+        movement.x = moveSpeed * Input.GetAxis("Horizontal");
+        movement.z = moveSpeed * Input.GetAxis("Vertical");
+        
+        //movement.x = Input.GetAxisRaw("Horizontal");
+        //movement.z = Input.GetAxisRaw("Vertical");
 
         anim.SetFloat("Horizontal", movement.x);
         anim.SetFloat("Vertical", movement.z);
@@ -23,6 +35,6 @@ public class playerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        //rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
