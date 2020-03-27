8ad7473078493caff93e570060b5fa5fca8edbf1
Joey Segura <10735119@my.uvu.edu>
2020-03-27 09:00:09 -0600

    Fixed playerMovment Script
    Changed charactercontroller to rb.velocity type movment.

--- unity3d-ronnyGame/Assets/Scripts/playerMovement.cs
+++ unity3d-ronnyGame/Assets/Scripts/playerMovement.cs
@@ -2,28 +2,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-[RequireComponent(typeof(CharacterController))]
 
 public class playerMovement : MonoBehaviour
 {
-    private CharacterController controller;
+    [SerializeField]
+    public Rigidbody rb;
+    //private CharacterController controller;
     private Vector3 movement;
     public float moveSpeed = 2f;
     public Animator anim;
     
-    //public Rigidbody rb;
-
     private void Start()
     {
-        controller = GetComponent<CharacterController>();
+        rb = GetComponent<Rigidbody>();
+        //controller = GetComponent<CharacterController>();
     }
 
     void Update()
     {
-        controller.Move(movement * Time.deltaTime);
+        movement = new Vector3(Input.GetAxisRaw("Horizontal")*moveSpeed, 0, Input.GetAxisRaw("Vertical")*moveSpeed);
+        rb.velocity = movement;
 
-        movement.x = moveSpeed * Input.GetAxis("Horizontal");
-        movement.z = moveSpeed * Input.GetAxis("Vertical");
+        //controller.Move(movement * Time.deltaTime);
+
+        //movement.x = moveSpeed * Input.GetAxis("Horizontal");
+        //movement.z = moveSpeed * Input.GetAxis("Vertical");
         
         //movement.x = Input.GetAxisRaw("Horizontal");
         //movement.z = Input.GetAxisRaw("Vertical");
