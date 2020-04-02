6f4ac99234e7de1cbfae6be843cad6e59d7c92cc
Joey Segura <10735119@my.uvu.edu>
2020-04-01 21:34:20 -0600

    displayText

---
+++ unity3d-ronnyGame/Assets/Scripts/displayText.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class displayText : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
