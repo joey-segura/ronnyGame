c87d7ed352bc96c79920af5af2e06f302e02ede5
Joey Segura <10735119@my.uvu.edu>
2020-04-20 21:03:07 -0600

    displayText Script
    Not working.

--- unity3d-ronnyGame/Assets/Scripts/displayText.cs
+++ unity3d-ronnyGame/Assets/Scripts/displayText.cs
@@ -3,13 +3,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class displayText : MonoBehaviour
 {
+
+    private void Start()
+    {
+        GameObject.Find("ritter");
+        //ritter.SetActive(false);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-            
+            //ritter.SetActive(true);
         }
     }
 
