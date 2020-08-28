b82107a93980d0a5e5ca7528dbdbf1dbba6f41f6
Joey Segura <10735119@my.uvu.edu>
2020-08-28 11:24:46 -0600

    SunCycle Scripts + Skybox Script
    SunCycle makes a sun sprite with a directional light rotate around the map always pointing at 0. Skybox script is intended to make the brightest part of the skybox rotate along with the sun but I haven't been able to get it to work yet.

---
+++ unity3d-ronnyGame/Assets/Scripts/SunCycle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunCycle : MonoBehaviour
+{
+    public float xRotation = -0.1f, yRotation = 0, zRotation = 0;
+
+    //public bool isDay = false;
+    
+    //Make sun travel slower during the day(?)
+    void Awake()
+    {
+
+    }
+        
+    void Update ()
+    {
+        transform.Rotate(xRotation, yRotation, zRotation);
+    }
+}
