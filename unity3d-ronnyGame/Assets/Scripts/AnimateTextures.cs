b2e910d72abc3c0098c849905930a68104435e5c
Joey Segura <10735119@my.uvu.edu>
2020-09-05 14:22:43 -0600

    HpBars&&AnimatedWater

---
+++ unity3d-ronnyGame/Assets/Scripts/AnimateTextures.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateTextures : MonoBehaviour
+{
+    public Renderer renderer;
+    public Texture[] textures;
+    public int currentTexture;
+
+    void Start()
+    {
+        StartCoroutine(AnimateTexture());
+    }
+    public IEnumerator AnimateTexture()
+    {
+        currentTexture++;
+        currentTexture %= textures.Length;
+        renderer.material.mainTexture = textures[currentTexture];
+        yield return new WaitForSeconds(.25f);
+        StartCoroutine(AnimateTexture());
+    }
+}
