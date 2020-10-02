0a40b132464871fa9103675878509b6e183adcbc
ritter <probsttanner@gmail.com>
2020-10-01 21:03:04 -0600

    Added cloud generator and cloud.cs to Joey Scene

---
+++ unity3d-ronnyGame/Assets/Scripts/Scene_Scripts/Cloud.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloud : MonoBehaviour
+{
+    public int id;
+    public Vector3 direction;
+    public float speed = 2.5f;
+    public CloudGenerator parentGenerator;
+
+    public void Awake()
+    {
+        parentGenerator = this.transform.GetComponentInParent<CloudGenerator>();
+    }
+    public void Update()
+    {
+        if (!parentGenerator.bound.Contains(this.transform.localPosition))
+        {
+            parentGenerator.RemoveCloud(id);
+            Destroy(this.gameObject);
+        }
+        if (direction != null)
+        {
+            this.transform.localPosition += (direction * (Time.deltaTime * speed));
+        }
+        
+    }
+}
