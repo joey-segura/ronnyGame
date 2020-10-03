d1fde84e9960fab40b216615b2049e2811f19b35
ritter <probsttanner@gmail.com>
2020-10-03 14:50:22 -0600

    Edited Playermovement.cs to include wall ride

--- unity3d-ronnyGame/Assets/Scripts/playerMovement.cs
+++ unity3d-ronnyGame/Assets/Scripts/playerMovement.cs
@@ -71,7 +71,79 @@ public class playerMovement : MonoBehaviour
             }
             else
             {
-                anim.SetBool("Moving", false);
+                //Start wallride operation (slip and slide!)
+                Vector3 reverse = newPos - this.transform.position;
+                Vector3 startingPos = new Vector3(newPos.x, newPos.y - 2.75f, newPos.z);
+                float angle = Mathf.Atan2(reverse.z, reverse.x) * Mathf.Rad2Deg;
+                float r = Mathf.Sqrt(reverse.x * reverse.x + reverse.z * reverse.z);
+                Vector3 posTest = new Vector3(r * Mathf.Cos((angle + 5) * Mathf.Deg2Rad) * -1, 0, r * Mathf.Sin((angle + 5) * Mathf.Deg2Rad) * -1);
+                Vector3 negTest = new Vector3(r * Mathf.Cos((angle - 5) * Mathf.Deg2Rad) * -1, 0, r * Mathf.Sin((angle - 5) * Mathf.Deg2Rad) * -1);
+                RaycastHit posHit;
+                RaycastHit negHit;
+                bool calc = false, pos = false, neg = false;
+                float dist1 = 0, dist2 = 0;
+                Vector3 point1 = Vector3.zero, point2 = Vector3.zero;
+
+                if (Physics.Raycast(startingPos, posTest.normalized, out posHit, 2f))
+                {
+                    dist1 = posHit.distance;
+                    point1 = posHit.point;
+                    pos = true;
+                }
+                if (Physics.Raycast(startingPos, negTest.normalized, out negHit, 2f))
+                {
+                    dist2 = negHit.distance;
+                    point2 = negHit.point;
+                    neg = true;
+                }
+
+                Vector3 lineVector = point2 - point1;
+                if (pos != neg)
+                {
+                    //Debug.LogError("oh shit!! backup ray!");
+                    RaycastHit playa;
+                    Debug.DrawRay(startingPos, new Vector3(reverse.x * -1, 0, reverse.z * -1).normalized, Color.yellow);
+                    if (Physics.Raycast(startingPos, new Vector3(reverse.x * -1, 0, reverse.z * -1).normalized, out playa, 1))
+                    {
+                        if (!pos)
+                        {
+                            dist1 = playa.distance;
+
+                            lineVector = point2 - playa.point;
+                        } else
+                        {
+                            dist2 = playa.distance;
+                            lineVector = playa.point - point1;
+                        }
+                    }
+                }
+                if (dist1 != 0 && dist2 != 0 && (dist1 + dist2) < 1f)
+                {
+                    anim.SetBool("Moving", true);
+                    calc = true;
+                }
+                //Debug.Log($"Dist1:{dist1} Dist2:{dist2}");
+                //Debug.Log(calc);
+                Vector3 finalPos = this.transform.position;
+
+                if (calc && (dist2 - dist1) > 0)
+                {
+                    finalPos -= lineVector.normalized * speed * Time.deltaTime;
+                } else if (calc)
+                {
+                    finalPos += lineVector.normalized * speed * Time.deltaTime;
+                } else
+                {
+                    anim.SetBool("Moving", false);
+                }
+                if (finalPos != this.transform.position)
+                {
+                    RaycastHit final;
+                    if (Physics.Raycast(finalPos, Vector3.down, out final, 4))
+                    {
+                        this.transform.position = finalPos;
+                    }
+                }
             }
         } else
         {
