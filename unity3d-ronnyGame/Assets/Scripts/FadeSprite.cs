2aca27e8132f3e917412589e09ea536e316a3298
ritter <probsttanner@gmail.com>
2020-10-07 23:18:02 -0600

    Added splash screen

---
+++ unity3d-ronnyGame/Assets/Scripts/FadeSprite.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeSprite : MonoBehaviour
+{
+    public float startingAlphaValue, endingAlphaValue, totalTime, timeElapsed;
+    public SpriteRenderer spr;
+    void Start()
+    {
+        spr = this.GetComponent<SpriteRenderer>();
+        if (spr == null)
+        {
+            Debug.LogError("Need sprite component!");
+        } else
+        {
+            spr.color = new Color(1, 1, 1, 0);
+            StartCoroutine(FadeIn());
+        }
+    }
+    public IEnumerator FadeIn()
+    {
+        yield return new WaitForSeconds(1);
+        while (timeElapsed < totalTime)
+        {
+            timeElapsed += Time.deltaTime;
+            float valueT = timeElapsed / totalTime;
+            spr.color = new Color(spr.color.r, spr.color.g, spr.color.b, Mathf.Lerp(startingAlphaValue, endingAlphaValue, Mathf.Sqrt(valueT)));
+            yield return new WaitForEndOfFrame();
+        }
+        yield return null;
+    }
+}
---
+++ unity3d-ronnyGame/Assets/Scripts/Scene_Scripts/SceneChange.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    public float seconds;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ChangeScene());
+    }
+    public IEnumerator ChangeScene()
+    {
+        yield return new WaitForSeconds(seconds);
+        SceneManager.LoadScene("Main_Menu");
+    }
+}
